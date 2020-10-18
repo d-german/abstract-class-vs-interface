826879462ed3c7e644c674de03c9fc63d72599e9
Damon German <damon.german@hyland.com>
2020-10-18 11:33:09 -0500

    made some changes

---
+++ AbstractClassVsInterface/Base.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AbstractClassVsInterface
+{
+    public abstract class Base
+    {
+        public abstract void MethodA();
+
+        public virtual void MethodB()
+        {
+            Console.WriteLine($"{nameof(MethodB)} implementation");
+        }
+    }
+
+    public class BaseImpl : Base
+    {
+        public override void MethodA()
+        {
+            Console.WriteLine($"{nameof(MethodA)} implementation");
+        }
+    }
+}
---
+++ AbstractClassVsInterface/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AbstractClassVsInterface
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+           Base b = new BaseImpl();
+
+           b.MethodA();
+           b.MethodB();
+        }
+    }
+}
